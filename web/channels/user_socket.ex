a269acb9068f75fdfd81b4d575ab0f0e8f743f8e
Hiroyuki Tanaka <ht0919@gmail.com>
2017-11-07 12:02:14 +0900

    Config for Heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Student.UserSocket do
   # channel "room:*", Student.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  #transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
