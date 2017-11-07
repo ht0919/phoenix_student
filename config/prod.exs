ebda531a6b12197deb39f5b4245a0e1608e7b5df
Hiroyuki Tanaka <ht0919@gmail.com>
2017-11-07 13:20:11 +0900

    Add elixir_buildpack.config

--- config/prod.exs
+++ config/prod.exs
@@ -13,13 +13,16 @@ use Mix.Config
 # which you typically run after static files are built.
 config :student, Student.Endpoint,
   load_from_system_env: true,
-  http: [:inet6, port: {:system, "PORT"}],
+  url: [scheme: "https", host: "evening-chamber-40802.herokuapp.com", port: 443],
+  #http: [:inet6, port: {:system, "PORT"}],
   #url: [host: "example.com", port: 80],
   #cache_static_manifest: "priv/static/cache_manifest.json"
-  url: [scheme: "https", host: "evening-chamber-40802.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
 
 # Configure your database
 config :student, Student.Repo,
@@ -27,9 +30,6 @@ config :student, Student.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
-
-# Do not print debug messages in production
-config :logger, level: :info
 
 # ## SSL Support
 #
